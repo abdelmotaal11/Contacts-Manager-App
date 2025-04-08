ef466f4f7d8409927418efc2a2f6c000aefa1464
mabdelmotal <mostafa6520@gmail.com>
2025-04-08 12:59:17 +0300

    Updating Repository.java & ContactDAO.java for adding ExecutorExecutor and Handler and livedata for

--- app/src/main/java/com/example/contactsmanager/Repository.java
+++ app/src/main/java/com/example/contactsmanager/Repository.java
@@ -1,6 +1,13 @@
 package com.example.contactsmanager;
 
+import android.os.Looper;
+
+import androidx.lifecycle.LiveData;
+
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.logging.Handler;
 
 public class Repository {
 
@@ -8,23 +15,43 @@ public class Repository {
 
     private final ContactDAO contactDAO;
 
+    //Used for Background Database Operations
+    Executor executor;
+    //Used for update UI
+    android.os.Handler handler;
+
 
     public Repository(ContactDAO contactDAO) {
         this.contactDAO = contactDAO;
+        this.executor = Executors.newSingleThreadExecutor();
+        this.handler = new android.os.Handler(Looper.getMainLooper());
     }
 
     //All methods from the DAO should be executed here
     public void addContact(Contacts contact){
-        contactDAO.insert(contact);
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactDAO.insert(contact);
+            }
+        });
+
+
     }
 
     public void deleteContact(Contacts contact)
     {
-        contactDAO.delete(contact);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactDAO.delete(contact);
+            }
+        });
+
     }
 
-    public List<Contacts> getAllContacts(){
-
+    public LiveData<List<Contacts>> getAllContacts(){
         return contactDAO.getAllContacts();
     }
 }
