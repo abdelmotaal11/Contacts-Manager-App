29544a5aa74ab218868752f24284890766330fbb
mabdelmotal <mostafa6520@gmail.com>
2025-04-09 15:32:17 +0300

    Initializing all components for the app and adding new Contact to for testing

--- app/src/main/java/com/example/contactsmanager/MainActivity.java
+++ app/src/main/java/com/example/contactsmanager/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.contactsmanager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contactsmanager.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //data source
+    private ContactDatabase contactDatabase;
+    private ArrayList<Contacts> contacts = new ArrayList<>();
+
+    //adapter
+    private MyAdapter myAdapter;
+
+    //Binding
+    private ActivityMainBinding mainBinding;
+    private MainActivityClickHandlers handlers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +42,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Data Binding
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        handlers = new MainActivityClickHandlers(this);
+        mainBinding.setClickHandler(handlers);
+
+        //RecyclerView
+        RecyclerView recyclerView = mainBinding.recyclerview;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        //adapter
+        myAdapter = new MyAdapter(contacts);
+
+        //dataabse
+        contactDatabase = ContactDatabase.getInstance(this);
+
+        //viewModel
+        MyViewModel viewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        //Inserting new contact for testing
+        Contacts c1 = new Contacts(1,"Mostafa","mostafa6520@gmail.com");
+        viewModel.addNewContact(c1);
+        //Load from DB
+        viewModel.getAllContacts().observe(this, new Observer<List<Contacts>>() {
+            @Override
+            public void onChanged(List<Contacts> contacts) {
+                for (Contacts c:contacts){
+                    Log.v("TAGY",c.getName());
+                }
+            }
+        });
+
+
+        recyclerView.setAdapter(myAdapter);
     }
 }
